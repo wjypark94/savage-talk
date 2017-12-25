f6657c8e6bc3664ac8b045d9b5ded78e933bace0
William Park <wjypark94@gmail.com>
2017-12-24 21:10:38 -0800

    Work in progress input box, working on components

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -15,9 +15,12 @@ class App extends React.Component {
         return (
             <div>
                 <h1> SAVAGE TALK </h1>
-                <button onClick> Submit </button>
+                <input value/>
+                <button onClick> Send </button>
+                <h2> Buddy List </h2>
                 <button onClick> Add Friend</button>
                 <button onClick> Delete Friend</button>
+                <h3> How do you feel </h3>
                 <button onClick> Emojis </button>
 
             </div>
