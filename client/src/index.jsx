44c11f19fe37d58cf2f477e334d88f474ba700a2
William Park <wjypark94@gmail.com>
2017-12-24 22:05:38 -0800

    Set up basic event handling with dummy data

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,22 +6,43 @@ class App extends React.Component {
     constructor(props) {
         super(props) 
             this.state = {
-                savedList: []
+                
             }
+        this.sendMessage = this.sendMessage.bind(this);
+        this.addFriend = this.addFriend.bind(this);
+        this.deleteFriend = this.deleteFriend.bind(this);
+        this.sendEmojis = this.sendEmojis.bind(this);
         
     }
+    sendMessage(){
+        console.log('Send message is working');
+    }
+
+    addFriend(){
+        console.log('Add a Friend is working');
+    }
+
+    deleteFriend(){
+        console.log('Delete Friend is working')
+    }
+
+    sendEmojis(){
+        console.log('Send an emoji is working')
+    }
+
+    
 
     render() {
         return (
             <div>
                 <h1> SAVAGE TALK </h1>
                 <input value/>
-                <button onClick> Send </button>
+                <button onClick={this.sendMessage}> Send </button>
                 <h2> Buddy List </h2>
-                <button onClick> Add Friend</button>
-                <button onClick> Delete Friend</button>
+                <button onClick={this.addFriend}> Add Friend</button>
+                <button onClick={this.deleteFriend}> Delete Friend </button>
                 <h3> How do you feel </h3>
-                <button onClick> Emojis </button>
+                <button onClick={this.sendEmojis}> Emojis </button>
 
             </div>
         )
